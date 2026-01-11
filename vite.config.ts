3f935f839af6a5524f1da225c253065a24995a12
Shishira B J <93389074+Inevitableshishira@users.noreply.github.com>
2026-01-11 15:43:13 +0530

    Refactor vite.config.ts for improved readability

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
+  },
   server: {
     host: true,
     port: 3000,
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     sourcemap: true,
-  }
+  },
 });
